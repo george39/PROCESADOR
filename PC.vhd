da958ba6cfeacdde2e01d2e7dde59257d2cd4795
george39 <george35@utp.edu.co>
2016-04-17 23:30:53 -0500

    modificacion al program counter

--- PC.vhd
+++ PC.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    18:55:00 04/09/2016 
--- Design Name: 
--- Module Name:    PC - arq_PC 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -30,26 +11,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity PC is
-    Port ( PC_IN : in  STD_LOGIC_VECTOR (31 downto 0);
-           rst : in  STD_LOGIC;
+    Port ( PCentrada : in  STD_LOGIC_VECTOR (31 downto 0);
+           reset : in  STD_LOGIC;
            clk : in  STD_LOGIC;
-           PC_OUT : out  STD_LOGIC_VECTOR (31 downto 0));
+           PCsalida : out  STD_LOGIC_VECTOR (31 downto 0));
 end PC;
 
-architecture arq_PC of PC is
+architecture arqProcesador of PC is
 
 begin
-     
-	    process(clk)
-		 begin
-		     
-			      if(rising_edge(clk))then
-					         if(rst = '1')then
-								        PC_OUT <= (others=>'0');
-					         else
-                	              PC_OUT <= PC_IN;
-                        end if;
-					end if;			
-        end process;					
-end arq_PC;
+
+process(clk)
+	begin
+		if(rising_edge(clk))then
+			if(reset = '1')then
+				PCsalida <= (others=>'0');
+			else
+				PCsalida <= PCentrada;
+			end if;
+		end if;
+	end process;
+
+end arqProcesador;
 
