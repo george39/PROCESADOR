562b337674111a33351f62a8326f02dee4be0b3f
george39 <george35@utp.edu.co>
2016-04-18 19:16:40 -0500

    modificacion al test bench de la memoria de instrucciones

--- tbmemoriaInstrucciones.vhd
+++ tbmemoriaInstrucciones.vhd
@@ -1,61 +1,35 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   21:05:46 04/09/2016
--- Design Name:   
--- Module Name:   C:/Users/GEORGE/Google Drive/utp/octavo semestre/arquitectura de computadores/procesador/memoriaInstrucciones/tbmemoriaInstrucciones.vhd
--- Project Name:  memoriaInstrucciones
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: memoriaInstrucciones
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use std.textio.all; 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
  
-ENTITY tbmemoriaInstrucciones IS
-END tbmemoriaInstrucciones;
+ENTITY tbinstructionMemory IS
+END tbinstructionMemory;
  
-ARCHITECTURE behavior OF tbmemoriaInstrucciones IS 
+ARCHITECTURE behavior OF tbinstructionMemory IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
-    COMPONENT memoriaInstrucciones
+    COMPONENT instructionMemory
     PORT(
          clk : IN  std_logic;
-         address : IN  std_logic_vector(3 downto 0);
+         MIentrada : IN  std_logic_vector(31 downto 0);
          reset : IN  std_logic;
-         outInstruction : OUT  std_logic_vector(31 downto 0)
+         MIsalida : OUT  std_logic_vector(31 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
    signal clk : std_logic := '0';
-   signal address : std_logic_vector(3 downto 0) := (others => '0');
+   signal MIentrada : std_logic_vector(31 downto 0) := (others => '0');
    signal reset : std_logic := '0';
 
  	--Outputs
-   signal outInstruction : std_logic_vector(31 downto 0);
+   signal MIsalida : std_logic_vector(31 downto 0);
 
    -- Clock period definitions
    constant clk_period : time := 20 ns;
@@ -63,11 +37,11 @@ ARCHITECTURE behavior OF tbmemoriaInstrucciones IS
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: memoriaInstrucciones PORT MAP (
+   uut: instructionMemory PORT MAP (
           clk => clk,
-          address => address,
+          MIentrada => MIentrada,
           reset => reset,
-          outInstruction => outInstruction
+          MIsalida => MIsalida
         );
 
    -- Clock process definitions
@@ -84,13 +58,11 @@ BEGIN
    stim_proc: process
    begin		
       -- hold reset state for 100 ns.
-              reset<='1';
-              address <= "0000";
-          wait for 100 ns;
-                    reset<='0';
-						  address <= "1110";
-			 
-
+      reset<='0';
+		MIentrada<= X"00000000";
+      wait for 100 ns;	
+		reset<='0';
+		MIentrada<= X"00000003";
       wait;
    end process;
 
