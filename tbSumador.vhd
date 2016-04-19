f1ee0b9d05bd893c6b28a0903c4967d2ce076a43
george39 <george35@utp.edu.co>
2016-04-18 19:32:35 -0500

    modificacion al test bench del sumador

--- tbSumador.vhd
+++ tbSumador.vhd
@@ -1,86 +1,68 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   15:08:13 04/10/2016
--- Design Name:   
--- Module Name:   C:/Users/GEORGE/Google Drive/utp/octavo semestre/arquitectura de computadores/procesador/sumador/tbSumador.vhd
--- Project Name:  sumador
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: sumador
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-
  
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
  
-ENTITY tbSumador IS
-END tbSumador;
+ENTITY tbsumador IS
+END tbsumador;
  
-ARCHITECTURE behavior OF tbSumador IS 
+ARCHITECTURE behavior OF tbsumador IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT sumador
     PORT(
-         op1 : IN  std_logic_vector(31 downto 0);
-         op2 : IN  std_logic_vector(31 downto 0);
-         resul : OUT  std_logic_vector(31 downto 0)
+         operando1 : IN  std_logic_vector(31 downto 0);
+         operando2 : IN  std_logic_vector(31 downto 0);
+         salidaSumador : OUT  std_logic_vector(31 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal op1 : std_logic_vector(31 downto 0) := (others => '0');
-   signal op2 : std_logic_vector(31 downto 0) := (others => '0');
+   signal operando1 : std_logic_vector(31 downto 0) := (others => '0');
+   signal operando2 : std_logic_vector(31 downto 0) := (others => '0');
 
  	--Outputs
-   signal resul : std_logic_vector(31 downto 0);
+   signal salidaSumador : std_logic_vector(31 downto 0);
    -- No clocks detected in port list. Replace <clock> below with 
    -- appropriate port name 
  
-   
+--   constant <clock>_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: sumador PORT MAP (
-          op1 => op1,
-          op2 => op2,
-          resul => resul
+          operando1 => operando1,
+          operando2 => operando2,
+          salidaSumador => salidaSumador
         );
 
-  
+   -- Clock process definitions
+--   <clock>_process :process
+   --begin
+--		<clock> <= '0';
+		--wait for <clock>_period/2;
+	--	<clock> <= '1';
+		--wait for <clock>_period/2;
+  -- end process;
  
 
    -- Stimulus process
    stim_proc: process
    begin		
-     op1<="00000000000000000000000000000001";
-	  op2<="00000000000000000000000000000011";
-         wait for 100 ns;
+      -- hold reset state for 100 ns.
+      operando1 <= X"00000001";
+		operando2 <= X"00000002";
+		
+		wait for 100 ns;	
 
-     
+     -- wait for <clock>_period*10;
 
+      -- insert stimulus here 
 
       wait;
    end process;
