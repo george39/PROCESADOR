5cdb4fbf098a881d73f66e55611632d7ce568ebb
george39 <george35@utp.edu.co>
2016-04-18 19:19:40 -0500

    modificacion al test bench del program counter

--- tbPC.vhd
+++ tbPC.vhd
@@ -1,30 +1,3 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   19:16:02 04/09/2016
--- Design Name:   
--- Module Name:   C:/Users/GEORGE/Google Drive/utp/octavo semestre/arquitectura de computadores/procesador/PC/tbPC.vhd
--- Project Name:  PC
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: PC
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
@@ -41,21 +14,21 @@ ARCHITECTURE behavior OF tbPC IS
  
     COMPONENT PC
     PORT(
-         PC_IN : IN  std_logic_vector(31 downto 0);
-         rst : IN  std_logic;
+         PCentrada : IN  std_logic_vector(31 downto 0);
+         reset : IN  std_logic;
          clk : IN  std_logic;
-         PC_OUT : OUT  std_logic_vector(31 downto 0)
+         PCsalida : OUT  std_logic_vector(31 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal PC_IN : std_logic_vector(31 downto 0) := (others => '0');
-   signal rst : std_logic := '0';
+   signal PCentrada : std_logic_vector(31 downto 0) := (others => '0');
+   signal reset : std_logic := '0';
    signal clk : std_logic := '0';
 
  	--Outputs
-   signal PC_OUT : std_logic_vector(31 downto 0);
+   signal PCsalida : std_logic_vector(31 downto 0);
 
    -- Clock period definitions
    constant clk_period : time := 20 ns;
@@ -64,10 +37,10 @@ BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: PC PORT MAP (
-          PC_IN => PC_IN,
-          rst => rst,
+          PCentrada => PCentrada,
+          reset => reset,
           clk => clk,
-          PC_OUT => PC_OUT
+          PCsalida => PCsalida
         );
 
    -- Clock process definitions
@@ -82,18 +55,17 @@ BEGIN
 
    -- Stimulus process
    stim_proc: process
-   begin	
-      
-      rst <='1';
-		PC_IN <= x"00000000";
-		rst <='0';
-      wait for 20 ns;
-      PC_IN	<= x"00000001";
-      wait for 20 ns;
-      PC_IN <= x"00000002";
-      wait for 20 ns;
-      PC_IN <= x"00000003";			
-     
+   begin		
+     reset <= '1';
+      wait for 100 ns;	
+		PCentrada <= x"00000000"; 
+      reset <= '0'; 
+		wait for 20 ns;
+		PCentrada <= x"00000001";
+		wait for 20 ns;
+		PCentrada<= x"00000002";
+		wait for 20 ns;
+		PCentrada<= x"00000003";
 
       wait;
    end process;
