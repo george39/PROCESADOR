f2784b027038c5285dd0dbc7bdba29e46948de45
george39 <george35@utp.edu.co>
2016-04-18 19:44:05 -0500

    modificacion del sumador

--- sumador.vhd
+++ sumador.vhd
@@ -1,26 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    14:59:10 04/10/2016 
--- Design Name: 
--- Module Name:    sumador - arqSumador 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -31,16 +11,19 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity sumador is
-    Port ( op1 : in  STD_LOGIC_VECTOR (31 downto 0);
-           op2 : in  STD_LOGIC_VECTOR (31 downto 0);
-           resul : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port ( operando1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           operando2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           salidaSumador : out  STD_LOGIC_VECTOR (31 downto 0));
 end sumador;
 
-architecture arqSumador of sumador is
+architecture arqProcesador of sumador is
 
 begin
 
-resul <= (op1 + op2);
+process(operando1,operando2)
+	begin
+		salidaSumador <= operando1 + operando2;
+	end process;
 
-end arqSumador;
+end arqProcesador;
 
