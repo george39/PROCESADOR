9ff6aa8a6cf7eddee2205c27095591adb35720c3
george39 <george35@utp.edu.co>
2016-04-18 19:36:51 -0500

    modificacion test bench unidad de control

--- tbUnidadControl.vhd
+++ tbUnidadControl.vhd
@@ -1,30 +1,3 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   00:01:13 04/14/2016
--- Design Name:   
--- Module Name:   C:/Users/GEORGE/Google Drive/utp/octavo semestre/arquitectura de computadores/procesador/unidadControl/tbUnidadControl.vhd
--- Project Name:  unidadControl
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: unidadControl
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
@@ -32,32 +5,28 @@ USE ieee.std_logic_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
  
-ENTITY tbUnidadControl IS
-END tbUnidadControl;
+ENTITY tbunidadControl IS
+END tbunidadControl;
  
-ARCHITECTURE behavior OF tbUnidadControl IS 
+ARCHITECTURE behavior OF tbunidadControl IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT unidadControl
     PORT(
+         clk : IN  std_logic;
          op : IN  std_logic_vector(1 downto 0);
          op2 : IN  std_logic_vector(2 downto 0);
          op3 : IN  std_logic_vector(5 downto 0);
          cond : IN  std_logic_vector(3 downto 0);
          icc : IN  std_logic_vector(3 downto 0);
-         enableMem : OUT  std_logic;
-         rfDest : OUT  std_logic;
-         rfSource : OUT  std_logic_vector(1 downto 0);
-         pcSource : OUT  std_logic_vector(1 downto 0);
-         wrEnMem : OUT  std_logic;
-         wrEnRF : OUT  std_logic;
          ALUOP : OUT  std_logic_vector(5 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
+   signal clk : std_logic := '0';
    signal op : std_logic_vector(1 downto 0) := (others => '0');
    signal op2 : std_logic_vector(2 downto 0) := (others => '0');
    signal op3 : std_logic_vector(5 downto 0) := (others => '0');
@@ -65,42 +34,39 @@ ARCHITECTURE behavior OF tbUnidadControl IS
    signal icc : std_logic_vector(3 downto 0) := (others => '0');
 
  	--Outputs
-   signal enableMem : std_logic;
-   signal rfDest : std_logic;
-   signal rfSource : std_logic_vector(1 downto 0);
-   signal pcSource : std_logic_vector(1 downto 0);
-   signal wrEnMem : std_logic;
-   signal wrEnRF : std_logic;
    signal ALUOP : std_logic_vector(5 downto 0);
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
- 
-   
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: unidadControl PORT MAP (
+          clk => clk,
           op => op,
           op2 => op2,
           op3 => op3,
           cond => cond,
           icc => icc,
-          enableMem => enableMem,
-          rfDest => rfDest,
-          rfSource => rfSource,
-          pcSource => pcSource,
-          wrEnMem => wrEnMem,
-          wrEnRF => wrEnRF,
           ALUOP => ALUOP
         );
 
-   
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
 
    -- Stimulus process
    stim_proc: process
    begin		
-      op <= "10";
+     
+op <= "10";
 		op2 <= "000";
 		op3 <= "000000" ;
 		cond <= "0000";
@@ -117,6 +83,8 @@ BEGIN
 		op3 <= "000000" ;
 		cond <= "1000";
 		icc <= "0000";		
+      wait;	  
+
       wait;
    end process;
 
