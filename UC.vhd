868ffed34663065af75dc57399c02e4d0e7a10a2
george39 <george35@utp.edu.co>
2016-04-19 22:57:57 -0500

    modificacion a la unidad de control

--- UC.vhd
+++ UC.vhd
@@ -1,4 +1,23 @@
-﻿library IEEE;
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:30:03 04/19/2016 
+-- Design Name: 
+-- Module Name:    UC - arqUC 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
