f047b5dc1fc6568b7b3951f3342fc93f457ddd3c
george39 <george35@utp.edu.co>
2016-04-20 22:47:42 -0500

    nueva modificacion a la ALU

--- ALU.vhd
+++ ALU.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
--- Uncomment the following library declaration if using
+--- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
