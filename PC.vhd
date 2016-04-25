5da0bd4a3ceedf4ecb6729512b309ad35fe32190
george39 <george35@utp.edu.co>
2016-04-24 21:40:53 -0500

    modificacion al PC

--- PC.vhd
+++ PC.vhd
@@ -11,13 +11,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity PC is
-    Port ( PCentrada : in  STD_LOGIC_VECTOR (31 downto 0);
+    Port ( PC_entrada : in  STD_LOGIC_VECTOR (31 downto 0);
            reset : in  STD_LOGIC;
            clk : in  STD_LOGIC;
-           PCsalida : out  STD_LOGIC_VECTOR (31 downto 0));
+           PC_salida : out  STD_LOGIC_VECTOR (31 downto 0));
 end PC;
 
-architecture arqProcesador of PC is
+architecture ArqProcesador of PC is
 
 begin
 
@@ -25,12 +25,12 @@ process(clk)
 	begin
 		if(rising_edge(clk))then
 			if(reset = '1')then
-				PCsalida <= (others=>'0');
+				PC_salida <= (others=>'0');
 			else
-				PCsalida <= PCentrada;
+				PC_salida <= PC_entrada;
 			end if;
 		end if;
 	end process;
 
-end arqProcesador;
+end ArqProcesador;
 
