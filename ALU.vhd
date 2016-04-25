cbfa678ec54546c3b98ef4a6f7ab29fc6ebfc99f
george39 <george35@utp.edu.co>
2016-04-24 21:32:59 -0500

    modificacion a al ALU

--- ALU.vhd
+++ ALU.vhd
@@ -1,7 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
----Uncomment the following library declaration if using
+
+-- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
@@ -13,46 +14,39 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity ALU is
     Port ( operando1 : in  STD_LOGIC_VECTOR (31 downto 0);
            operando2 : in  STD_LOGIC_VECTOR (31 downto 0);
-           aluOP : in  STD_LOGIC_VECTOR (5 downto 0);
-          -- carry : in  STD_LOGIC;
-           AluResult : out  STD_LOGIC_VECTOR (31 downto 0));
+           alu_op : in  STD_LOGIC_VECTOR (5 downto 0);
+           salida_alu : out  STD_LOGIC_VECTOR (31 downto 0));
 end ALU;
 
-architecture arqProcesador of ALU is
+architecture ArqProcesador of ALU is
 
 begin
 
-  process(operando1,operando2,aluOP)--,carry)
-	begin
-	   case (aluOP) is 
-		
-		   when "000001" => --and
-				AluResult <= operando1 and operando2;
-		
-		   when "000101" => --addn
-				AluResult <= operando1 nand operando2;
-		
-			when "000000" => -- add
-				AluResult <= operando1 + operando2;
-			
-			when "001000" => -- or
-				AluResult <= operando1 or operando2;
-			
-                        when "000110" => -- orn
-				AluResult <= operando1 nor operando2;
-			
-			when "000100" => -- sub
-				AluResult <= operando1 - operando2;
-			
-			when "000011" => -- xor
-				AluResult <= operando1 xor operando2;
-			
-			when "000111" => -- xorn
-				AluResult <= operando1 xnor operando2;
-			
-			when others => -- Cae el nop
-				AluResult <= (others=>'0');
+  process(operando1,operando2,alu_op)
+  begin
+	   case (alu_op) is
+		   when "000000" => -- ADD
+				salida_alu <= operando1 + operando2;
+			when "000001" => -- AND
+				salida_alu <= operando1 and operando2;
+			when "000010" => -- OR
+				salida_alu <= operando1 or operando2;
+			when "000011" => -- XOR
+				salida_alu <= operando1 xor operando2;
+			when "000100" => -- SUB
+				salida_alu <= operando1 - operando2;
+			when "000101" => -- ANDN
+				salida_alu <= operando1 nand operando2;
+			when "000110" => -- ORN
+				salida_alu <= operando1 nor operando2;
+			when "000111" => -- XORN
+				salida_alu <= operando1 xnor operando2;
+			when others => 
+				salida_alu <= (others=>'0');
 		end case;
-	end process;
-end arqProcesador;
+  end process;
+
+end ArqProcesador;
+
+
 
