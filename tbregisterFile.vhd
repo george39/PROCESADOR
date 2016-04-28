e04e3a13edc6167cf68864552e09db097c703ef2
george39 <george35@utp.edu.co>
2016-04-28 00:02:13 -0500

    nuevo test bench del RF

--- tbregisterFile.vhd
+++ tbregisterFile.vhd
@@ -1,30 +1,3 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   21:47:11 04/17/2016
--- Design Name:   
--- Module Name:   C:/Users/GEORGE/Google Drive/utp/octavo semestre/arquitectura de computadores/procesador/proyectoProcesador/tbregisterFile.vhd
--- Project Name:  proyectoProcesador
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: registerFile
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
@@ -32,111 +5,83 @@ USE ieee.std_logic_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
  
-ENTITY tbregisterFile IS
-END tbregisterFile;
+ENTITY tb_RF IS
+END tb_RF;
  
-ARCHITECTURE behavior OF tbregisterFile IS 
+ARCHITECTURE behavior OF tb_RF IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
-    COMPONENT registerFile
+    COMPONENT RF
     PORT(
-         clk : IN  std_logic;
          reset : IN  std_logic;
-         rs1 : IN  std_logic_vector(5 downto 0);
-         rs2 : IN  std_logic_vector(5 downto 0);
-         rd : IN  std_logic_vector(5 downto 0);
-         writeEnable : IN  std_logic;
-         dataToWrite : IN  std_logic_vector(31 downto 0);
+         rs1 : IN  std_logic_vector(4 downto 0);
+         rs2 : IN  std_logic_vector(4 downto 0);
+         rd : IN  std_logic_vector(4 downto 0);
+         salida_alu : IN  std_logic_vector(31 downto 0);
          crs1 : OUT  std_logic_vector(31 downto 0);
-         crs2 : OUT  std_logic_vector(31 downto 0);
-         crd : OUT  std_logic_vector(31 downto 0)
+         crs2 : OUT  std_logic_vector(31 downto 0)
         );
     END COMPONENT;
     
 
    --Inputs
-   signal clk : std_logic := '0';
    signal reset : std_logic := '0';
-   signal rs1 : std_logic_vector(5 downto 0) := (others => '0');
-   signal rs2 : std_logic_vector(5 downto 0) := (others => '0');
-   signal rd : std_logic_vector(5 downto 0) := (others => '0');
-   signal writeEnable : std_logic := '0';
-   signal dataToWrite : std_logic_vector(31 downto 0) := (others => '0');
+   signal rs1 : std_logic_vector(4 downto 0) := (others => '0');
+   signal rs2 : std_logic_vector(4 downto 0) := (others => '0');
+   signal rd : std_logic_vector(4 downto 0) := (others => '0');
+   signal salida_alu : std_logic_vector(31 downto 0) := (others => '0');
 
  	--Outputs
    signal crs1 : std_logic_vector(31 downto 0);
    signal crs2 : std_logic_vector(31 downto 0);
-   signal crd : std_logic_vector(31 downto 0);
-
-   -- Clock period definitions
-   constant clk_period : time := 10 ns;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+--   constant <clock>_period : time := 10 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
-   uut: registerFile PORT MAP (
-          clk => clk,
+   uut: RF PORT MAP (
           reset => reset,
           rs1 => rs1,
           rs2 => rs2,
           rd => rd,
-          writeEnable => writeEnable,
-          dataToWrite => dataToWrite,
+          salida_alu => salida_alu,
           crs1 => crs1,
-          crs2 => crs2,
-          crd => crd
+          crs2 => crs2
         );
 
    -- Clock process definitions
-   clk_process :process
-   begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
-   end process;
+--   <clock>_process :process
+--   begin
+--		<clock> <= '0';
+--		wait for <clock>_period/2;
+--		<clock> <= '1';
+--		wait for <clock>_period/2;
+--   end process;
  
 
    -- Stimulus process
    stim_proc: process
    begin		
-   rs1<="000001" ;
-   rs2 <="000010";
-   rd <="000001";
-   reset<='1';
-   writeEnable <='1';
-   dataToWrite <=(others => '1');
+      reset<='1';
+		-- hold reset state for 100 ns.
+      rs1 <="00001";
+      rs2 <="00010";
+		rd <="00001";
+		salida_alu <= X"00000000";
+		wait for 100 ns;
+		
+		rs1 <="00001";
+      rs2 <="00010";
+		rd <="00001";
+		salida_alu <= X"00000004";
+		wait for 100 ns;
+		
 
-      wait for 100 ns;	
-
-
-	rs1<="000001" ;
-   rs2 <="000010";
-   rd <= "000001";
-   reset<='0';
-   writeEnable <='0';
-   dataToWrite <=(others => '1');
-
-      wait for 100 ns;	
-     
-	 rs1<="000001" ;
-   rs2 <="000010";
-   rd <= "000011";
-   reset<='0';
-   writeEnable <='1';
-   dataToWrite <=(others => '1');
-
-      wait for 100 ns;	
- rs1<="000011" ;
-   rs2 <="000110";
-   rd <= "000010";
-   reset<='0';
-   writeEnable <='1';
-   dataToWrite <=(others => '1');
-
-      wait for 100 ns;	
- 
+      -- insert stimulus here 
 
       wait;
    end process;
