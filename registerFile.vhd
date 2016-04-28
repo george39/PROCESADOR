e6f85f83ac619524763dc29f741d55a900d2f532
george39 <george35@utp.edu.co>
2016-04-27 23:51:51 -0500

    modificacion al registerFile

--- registerFile.vhd
+++ registerFile.vhd
@@ -1,6 +1,8 @@
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -10,44 +12,38 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity registerFile is
-    Port ( clk : in  STD_LOGIC;
-           reset : in  STD_LOGIC;
-           rs1 : in  STD_LOGIC_VECTOR (5 downto 0);
-           rs2 : in  STD_LOGIC_VECTOR (5 downto 0);
-           rd : in  STD_LOGIC_VECTOR (5 downto 0);
-           writeEnable : in  STD_LOGIC;
-           dataToWrite : in  STD_LOGIC_VECTOR (31 downto 0);
+entity RF is
+    Port ( reset : in  STD_LOGIC;
+           rs1 : in  STD_LOGIC_VECTOR (4 downto 0);
+           rs2 : in  STD_LOGIC_VECTOR (4 downto 0);
+           rd : in  STD_LOGIC_VECTOR (4 downto 0);
+           salida_alu : in  STD_LOGIC_VECTOR (31 downto 0);
            crs1 : out  STD_LOGIC_VECTOR (31 downto 0);
-           crs2 : out  STD_LOGIC_VECTOR (31 downto 0);
-           crd : out  STD_LOGIC_VECTOR (31 downto 0));
-end registerFile;
+           crs2 : out  STD_LOGIC_VECTOR (31 downto 0));
+end RF;
 
-architecture arqProcesador of registerFile is
+architecture ArqProcesador of RF is
 
 type ram_type is array (0 to 39) of std_logic_vector (31 downto 0);
 	signal registers : ram_type := (others => x"00000000");
 
 begin
-  --,reset,registerSource1,registerSource2,registerDestination,writeEnable,dataToWrite
-	process(reset,rs1,rs2,rd,writeEnable,dataToWrite)--clkFPGA)
+   
+	process(reset,rs1,rs2,rd,salida_alu)
 	begin
-		--if(rising_edge(clkFPGA))then
-			if(reset = '1')then
-				crs1 <= (others=>'0');
-				crs2 <= (others=>'0');
-				crd <= (others=>'0');
-				registers <= (others => x"00000000");
-			else
-				crs1 <= registers(conv_integer(rs1));
-				crs2 <= registers(conv_integer(rs2));
-				crd <= registers(conv_integer(rd));
-				if(writeEnable = '1' and rd /= "000000")then
-					registers(conv_integer(rd)) <= dataToWrite;
-				end if;
+	   
+		if(reset = '1')then
+			crs1 <= (others=>'0');
+			crs2 <= (others=>'0');
+			registers <= (others => x"00000000");
+		else
+			crs1 <= registers(conv_integer(rs1));
+			crs2 <= registers(conv_integer(rs2));
+			if (rd /= "00000") then 
+			   registers (conv_integer(rd)) <= salida_alu;
 			end if;
-		--end if;
+		end if;
 	end process;
 
-end arqProcesador;
+end ArqProcesador;
 
